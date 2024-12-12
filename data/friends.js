37f40b10b3ad1669b6e819230279ed882ce442be
olha-mykytka <olhamykytka00@gmail.com>
2024-12-11 17:43:07 -0700

    Add files via upload

---
+++ data/friends.js
@@ -0,0 +1,49 @@
+export const friends = [
+  {
+    "first": "Automatic Pet Feeder",
+    "photo": "bmerritt.jpg",
+    "bornYear": "$49.99",
+    "employed": "Provide pets with extra love, attention, and care with our automatic feeder, featuring scheduled meal times and customizable meal portions.",
+    "hobbies": [
+      "Feeding schedule",
+      "Portion control",
+      "Wi-Fi connection",
+      "Twist-lock lid",
+      "Voice recorder"
+    ]
+  },
+  {
+    "first": "Pet Camera with Treat Dispenser",
+    "photo": "mthomas.jpg",
+    "bornYear": "$189.99",
+    "employed": "Keep your pet entertained all day and all night with the pet camera treat dispenser.",
+    "hobbies": [
+      "360 full room coverage",
+      "HD Video Quality",
+      "Automatically rotates",
+      "Tracking",
+      "100 treats capacity dispenser"
+    ]
+  },
+  {
+    "first": "Ergonomic Pet Brush",
+    "photo": "shoopes.jpg",
+    "bornYear": "$19.99",
+    "employed": "Stainless steel pins are cushioned to ensure that pets are brushed comfortably, and the rounded design promotes more comfortable brushing.",
+    "hobbies": [
+      "Stainless Steel pins",
+      "Strong and durable"
+    ]
+  },
+  {
+    "first": "Portable Water Bottle for Pets",
+    "photo": "eheiner.jpg",
+    "bornYear": "$22.99",
+    "employed": "Our Portable water bottle for pets help make it simple to carry water for your dog no matter where you go.",
+    "hobbies": [
+      "Capacity 18 oz",
+      "Lightweights",
+      "Carrying rope"
+    ]
+  }
+]
